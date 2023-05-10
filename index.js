addc3fbb18eeaf195c5c870a001bc0e26aa478a5
dg1223 <alavi1223@hotmail.com>
2023-05-10 19:37:13 -0400

    create and update db

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
+
+// sets POST request header, content-type, body etc.
 app.use(express.json());
 
 // configure MongoDB
-const { MongoClient, ServerApiVersion } = require("mongodb");
+
 const uri =
-  "mongodb+srv://alavi1223:<password>@dg1223.za2ri3i.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://alavi1223:Ro1zEwJaDfcJXchr@dg1223.za2ri3i.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -25,6 +28,27 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const database = client.db("usersDB");
+    const userCollection = database.collection("users");
+
+    // GET
+    app.get("/users", async (req, res) => {
+      const cursor = userCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // POST
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      console.log("new user: ", user);
+
+      // store data in DB
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -32,7 +56,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
